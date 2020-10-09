84c45ddbb9c5200144698c3d1d2163eb592b2023
BjornFJohansson <bjorn_johansson@bio.uminho.pt>
2020-10-09 13:44:03 +0100

    last

--- minaj/minaj.py
+++ minaj/minaj.py
@@ -9,51 +9,56 @@ del get_versions
 def main():
     import sys
     pyver = sys.version[:6]
-    
+
     import os
-    
+
     def cmd_exists(cmd):
         return any(
-                os.access(os.path.join(path, cmd), os.X_OK) 
+                os.access(os.path.join(path, cmd), os.X_OK)
                 for path in os.environ["PATH"].split(os.pathsep))
 
     print('''
-               _             _ 
+               _             _
               (_)           (_)                      .-=-.          .--.
      _ __ ___  _ _ __   __ _ _           __        .'     '.       /  " )
     | '_ ` _ \| | '_ \ / _` | |  _     .'  '.     /   .-.   \     /  .-'\\
     | | | | | | | | | | (_| | | ( \   / .-.  \   /   /   \   \   /  /    ^
     |_| |_| |_|_|_| |_|\__,_| |  \ `-` /   \  `-'   /     \   `-`  /
                            _/ |   `-.-`     '.____.'       `.____.'
-                          |__/ 
+                          |__/
     minaj version  {}
     minaj is running on Python {}
-    
+
     conda           is available: {}
     anaconda-client is available: {}
-    
+
     This script helps you to build, convert and upload a conda package.
     This script should be run in the same folder as the meta.yaml file.
-    
+
     You will have the following options:
-    
+
     - build or process already built packages
     - set python version to use for build
-    - convert or not to other platforms
+    - convert to other platforms
     - upload to your conda channel
-    
+
     for uploading packages, you need to have anaconda-client installed.
     it can be installed like this:
-    
+
     conda install anaconda-client
-    
-    '''.format(__version__, 
-               pyver, 
-               cmd_exists("conda"), 
+
+    To build noarch packages, add this to the build section of the meta.yaml
+
+    build:
+      noarch: python
+
+    '''.format(__version__,
+               pyver,
+               cmd_exists("conda"),
                cmd_exists("anaconda")))
-         
+
     import subprocess
-    
+
     if sys.version_info[0] < 3:
         get_input = raw_input
     else:
@@ -64,34 +69,34 @@ def main():
     except ImportError:
         def colored(text, color): return text
 
-    try: 
+    try:
         import pathlib
     except ImportError:
         import pathlib2 as pathlib
-    
+
     if not pathlib.Path("meta.yaml").exists():
         sys.exit(colored("No meta.yaml file found in this directory.\n", "red"))
-    
+
     ver = get_input("Which python version do you want to build with (hit return for default {}) ? ".format(pyver))
-    if not ver: 
-        ver=pyver  
-    
-    bashCommand = "conda build . --output --python={ver}".format(ver=ver) 
+    if not ver:
+        ver=pyver
+
+    bashCommand = "conda build . --output --python={ver}".format(ver=ver)
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    
+
     pkgpth   = pathlib.Path(process.communicate()[0].strip())
     pkgname  = pkgpth.name
     builddir = pkgpth.parent.parent
-    
+
     print(bashCommand)
-    
+
     #print(colored("Build dir: ","magenta") + colored( builddir, "blue"))
-    print(colored("Package  : ","magenta") + colored( pkgpth  , "blue"))
+    print(colored("Package  : ","magenta") + colored( str(pkgpth)  , "blue"))
 
     response = get_input("build this package (y/n) ? ")
-    
-    if response.lower().startswith("y"):       
-        bashCommand = "conda build . --no-anaconda-upload --python={ver}".format(ver=ver) 
+
+    if response.lower().startswith("y"):
+        bashCommand = "conda build . --no-anaconda-upload --python={ver}".format(ver=ver)
         process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
         output = process.communicate()[0]
         print(colored(output, "blue"))
@@ -108,7 +113,7 @@ def main():
     else: # ask to convert
         for platform in ["win-64", "osx-64", "win-32", "linux-32", "linux-64"]:
             response = get_input("convert to {} (y/n) ?".format(platform))
-            if response.lower().startswith("y"):           
+            if response.lower().startswith("y"):
                 bashCommand = "conda convert {} -p {} -o {}".format(str(pkgpth), platform, str(builddir))
                 print(colored(bashCommand, "cyan"))
                 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, universal_newlines=True)
@@ -116,21 +121,21 @@ def main():
                 print(colored(output, "blue"))
             else:
                 print(colored("convert skipped.", "red"))
-                
+
     pts=[]
-    
+
     print()
     print("The following packages have been created:")
     print()
-    
+
     for dir_ in ["win-64", "osx-64", "win-32", "linux-32", "linux-64"]:
         upload_pth = builddir.joinpath(dir_, pkgname)
         if upload_pth.exists():
             print(colored(upload_pth, "blue"))
             pts.append(upload_pth)
-        
+
     response = get_input("upload these package(s) (y/n) ? ")
-    
+
     if response.lower().startswith("y"):
         response = get_input("label (hit return for default 'main')?")
         label = response or "main"
@@ -141,15 +146,15 @@ def main():
             print(colored(output, "blue"))
     else:
         print(colored("upload skipped.", "red"))
-        
-    process = subprocess.Popen("conda build purge".split(), stdout=subprocess.PIPE, universal_newlines=True) 
+
+    process = subprocess.Popen("conda build purge".split(), stdout=subprocess.PIPE, universal_newlines=True)
     output = process.communicate()[0]
     print(output)
-    print('''                  
+    print('''
                                ____
       ________________________/ O  \___/
-     <_/_\_/_\_/_\_/_\_/_\_/_______/   \   zzzzz done!''')
-    
-    
+     <_/_\_/_\_/_\_/_\_/_\_/_______/   \   zzzzz !''')
+
+
 if __name__=="__main__":
     main()
